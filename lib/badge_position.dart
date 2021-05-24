e2003c6691edf17085bece1f64757517333f7641
Ivan Terekhin <i.terhin@gmail.com>
2021-05-24 13:51:42 +0300

    Null safety migration

--- lib/badge_position.dart
+++ lib/badge_position.dart
@@ -1,10 +1,10 @@
 /// The [BadgePosition] class represents position of badge on icon
 /// Has predefined positions for every angle
 class BadgePosition {
-  final double top;
-  final double right;
-  final double bottom;
-  final double left;
+  final double? top;
+  final double? right;
+  final double? bottom;
+  final double? left;
 
   const BadgePosition({this.top, this.right, this.bottom, this.left});
 
